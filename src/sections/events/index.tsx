735ce376203677b1376b16b7649787d1103df996
Vedant Chainani <vedantchainani1084@gmail.com>
2024-02-24 04:57:02 +0530

    test

--- src/sections/events/index.tsx
+++ src/sections/events/index.tsx
@@ -5,7 +5,7 @@ import { EventList } from '~/components/event-card';
 const Events = () => {
   return (
     <section id='events' className=''>
-      <div className='relative mx-auto flex w-full items-center justify-center overflow-x-hidden'>
+      <div className='relative mx-auto flex w-full items-center justify-center overflow-hidden'>
         <video
           controls={false}
           autoPlay
